518794d4f275434ec389042535203c406b4b5de4
tsaunders-gsa <terry.saunders@gsa.gov>
2020-03-09 14:49:59 -0400

    Added Example Files

--- ERIS/Program.cs
+++ ERIS/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Configuration;
 
 namespace ERIS
 {
     class Program
     {
+        //Reference to logger
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        //File paths from config file
+        //private static string hrFilePath = ConfigurationManager.AppSettings["HRFILE"].ToString();
+
+        //private static string separationFilePath = ConfigurationManager.AppSettings["SEPARATIONFILE"].ToString();
+
+        //Stopwatch objects
+        private static Stopwatch timeForApp = new Stopwatch();
+
+        private static Stopwatch timeForProcess = new Stopwatch();
+
+        public static object ConfigurationManager { get; private set; }
+
         static void Main(string[] args)
         {
+            //Start timer
+            timeForApp.Start();
+
+            //Log start of application
+            log.Info("Application Started: " + DateTime.Now);
+
+            //Log action
+            log.Info("Processing HR Monster File:" + DateTime.Now);
+
+            log.Info("Done Processing HR Links File(s):" + DateTime.Now);
+
+            log.Info("Sending Summary File");
+            
+            log.Info("Summary file sent");
+
+            //Stop second timer
+            timeForApp.Stop();
+
+            //Log total time
+            log.Info(string.Format("Application Completed in {0} milliseconds", timeForApp.ElapsedMilliseconds));
+
+            //Log application end
+            log.Info("Application Done: " + DateTime.Now);
         }
     }
 }
