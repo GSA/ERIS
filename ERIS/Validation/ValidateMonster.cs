070fd8c83444dec462b4a127c62818f91f8b7f83
tsaunders-gsa <terry.saunders@gsa.gov>
2020-03-09 15:02:23 -0400

    Added Folders and Classes needed.

---
+++ ERIS/Validation/ValidateMonster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ERIS.Validation
+{
+    internal class ValidateMonster
+    {
+
+    }
+}
