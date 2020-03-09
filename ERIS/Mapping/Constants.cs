f1ab0f42c38251ba274675b2784405a7bb90c350
tsaunders-gsa <terry.saunders@gsa.gov>
2020-03-09 15:39:19 -0400

    Updated Constants.  This still needs to be looke over.

--- ERIS/Mapping/Constants.cs
+++ ERIS/Mapping/Constants.cs
@@ -8,5 +8,33 @@ namespace ERIS.Mapping
 {
     internal static class MonsterConstants
     {
+        public const int EMPLOYEE_LAST_NAME = 0;
+        public const int EMPLOYEE_MIDDLE_NAME = 2;        
+        public const int EMPLOYEE_FIRST_NAME = 3;
+        public const int EMPLOYEE_SUFFIX = 4;
+        public const int SOCIAL_SECURITY_NUMBER = 5;
+        public const int DATE_OF_BIRTH = 6;
+        public const int GENDER = 7;
+        public const int REGION = 8;
+        public const int OFFICE_SYMBOL = 9;
+        public const int MAJOR_ORG = 10;
+        public const int VIRTUAL = 11;
+        public const int VIRTUAL_REGION = 12;
+        public const int JOB_TITLE = 13;
+        public const int HOME_ADDRESS_1 = 14;
+        public const int HOME_ADDRESS_2 = 15;
+        public const int HOME_CITY = 16;
+        public const int HOME_STATE = 17;
+        public const int HOME_ZIP_CODE = 18;
+        public const int HOME_COUNTRY = 19;
+        public const int HOME_PHONE = 20;
+        public const int WORK_CELL = 21;
+        public const int PERSONAL_EMAIL = 22;
+        public const int BUILDING_LOCATION_CODE = 23;
+        public const int BIRTH_CITY = 24;
+        public const int BIRTH_STATE = 25;
+        public const int BIRTH_COUNTRY = 26;
+        public const int COUNTRY_OF_CITIZENSHIP = 27;
+        public const int CITIZEN = 28;        
     }
 }
