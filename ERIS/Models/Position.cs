6819760527547c0b332360af5b1545bfb1702d88
tsaunders-gsa <terry.saunders@gsa.gov>
2020-03-09 15:12:16 -0400

    Added Models

---
+++ ERIS/Models/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERIS.Models
+{
+    class Position
+    {
+        public string JobTitle { get; set; }
+        public string Region { get; set; }
+        public bool IsVirtual { get; set; }
+        public bool VirtualRegion { get; set; }
+        public string OfficeSymbol { get; set; }
+        public string MajorOrg { get; set; }       
+    }
+}
