5b280a0654c46f817ae270603bee6eb7c7255cd0
tsaunders-gsa <terry.saunders@gsa.gov>
2020-03-10 09:30:23 -0400

    Added helpers and started adding process section.

---
+++ ERIS/Utilities/Helpers.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ERIS.Utilities
+{
+    internal static class Helpers
+    {
+        /// <summary>
+        /// Hashes SSN
+        /// </summary>
+        /// <param name="ssn"></param>
+        /// <returns></returns>
+        public static byte[] HashSsn(string ssn)
+        {
+            byte[] hashedFullSsn = null;
+
+            SHA256 shaM = new SHA256Managed();
+
+            ssn = ssn.Replace("-", string.Empty);
+
+            using (shaM)
+            {
+                hashedFullSsn = shaM.ComputeHash(Encoding.UTF8.GetBytes(ssn));
+            }
+
+            return hashedFullSsn;
+        }
+    }
+}
